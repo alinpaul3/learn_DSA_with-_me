00d2fd1f817f109a57abee5faf28e5f46a2d64f0
Alina Paul <alinaa20@gmail.com>
2024-08-20 07:16:30 +0530

    Removed class files

---
+++ singlylinkedlist.java
@@ -0,0 +1,13 @@
+public class SinglyLinkedList{
+    private ListNode head;
+    private static class  ListNode{
+      private int data;
+      private ListNode next; 
+      public ListNode(int data){
+        this.data=data;
+        this.next=null;
+      } 
+    }
+
+
+}
