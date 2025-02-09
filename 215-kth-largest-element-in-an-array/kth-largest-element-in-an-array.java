7f82f62512a5ad984fa8577972a91e17eec6a387
Alina Paul <145111333+alinpaul3@users.noreply.github.com>
2025-02-10 01:05:22 +0530

    Time: 63 ms (31.47%) | Memory: 61.9 MB (14.26%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,44 +1,14 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        return quickSelect(nums, 0, n - 1, n - k);
-    }
+   public int findKthLargest(int[] nums, int k) {
+    PriorityQueue<Integer> pq = new PriorityQueue<>(); // Min-Heap
 
-    private int quickSelect(int[] nums, int left, int right, int k) {
-        if (left == right) return nums[left]; // Only one element left
-
-        Random rand = new Random();
-        int pivotIndex = left + rand.nextInt(right - left + 1); // Random pivot to avoid worst-case
-        pivotIndex = partition(nums, left, right, pivotIndex);
-
-        if (k == pivotIndex) {
-            return nums[k];
-        } else if (k < pivotIndex) {
-            return quickSelect(nums, left, pivotIndex - 1, k);
-        } else {
-            return quickSelect(nums, pivotIndex + 1, right, k);
+    for (int num : nums) {
+        pq.offer(num); // Add number to heap
+        if (pq.size() > k) {
+            pq.poll(); // Remove smallest element
         }
     }
 
-    private int partition(int[] nums, int left, int right, int pivotIndex) {
-        int pivotValue = nums[pivotIndex];
-        swap(nums, pivotIndex, right); // Move pivot to the end
-        int storeIndex = left;
-
-        for (int i = left; i < right; i++) {
-            if (nums[i] < pivotValue) {
-                swap(nums, storeIndex, i);
-                storeIndex++;
-            }
-        }
-
-        swap(nums, storeIndex, right); // Move pivot to its final place
-        return storeIndex;
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-  }
+    return pq.peek(); // Top element is the Kth largest
+}
+}
