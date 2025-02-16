e81e5a04c29bb7976298f4cea0a66669e776aa14
Alina Paul <145111333+alinpaul3@users.noreply.github.com>
2025-02-16 13:41:10 +0530

    Time: 30 ms (7.53%) | Memory: 45.2 MB (59.07%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left=1,right=getmax(piles);
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(gethours(piles,h,mid)){
+                right=mid;
+            }
+            else{
+                left=mid+1;
+            }
+            }return left;}
+        private boolean gethours(int[] piles,int h, int k){
+            int hours=0;
+            for(int pile:piles){
+                hours+=(int)Math.ceil((double)pile/k);//used to get the upperbound
+            }
+            return hours<=h;
+        }
+        private int getmax(int[] piles){//get the max bananas to complete 
+            int max=0;
+            for(int pile:piles){
+                max=Math.max(max,pile);
+            }
+            return max;
+        }
+}
+
+// Time Complexity	O(n log max(piles))
+// Space Complexity	O(1)
