25e0df37148b667ea3c572fa1ef5bfca2d856f13
Alina Paul <145111333+alinpaul3@users.noreply.github.com>
2025-08-11 19:36:07 +0530

    Time: 29 ms (78.25%) | Memory: 55.5 MB (52.75%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,23 @@
+class StockSpanner {
+    private Stack<int[]> stack; // each element is {price, span}
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+        // Merge spans of previous days with smaller or equal prices
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+        stack.push(new int[]{price, span});
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
