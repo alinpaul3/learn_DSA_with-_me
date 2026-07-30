68b4a449d39c8f4dfd915664a3e98a15c6e5e2bd
Alina Paul <145111333+alinpaul3@users.noreply.github.com>
2026-07-30 12:37:36 +0530

    Time: 2 ms (59.91%) | Memory: 43.1 MB (53.50%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,23 @@
+//Stack solution
+
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+
+    private String build(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c != '#') {
+                stack.push(c);
+            } else if (!stack.isEmpty()) {
+                stack.pop();
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
