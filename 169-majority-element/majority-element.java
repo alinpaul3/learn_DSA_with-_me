053ed154430ae5e88f27878c152ffae9a3678025
Alina Paul <145111333+alinpaul3@users.noreply.github.com>
2026-08-05 11:31:31 +0530

    Time: 15 ms (24.34%) | Memory: 52.3 MB (97.41%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,11 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-    for(int i=0;i<nums.length;i++){
-        map.merge(nums[i], 1, Integer::sum);
-}
-    int maxKey = Collections.max(map.entrySet(), 
-                  Map.Entry.comparingByValue()).getKey(); 
-    return maxKey; 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]))
+                map.put(nums[i], map.get(nums[i]) + 1);
+            else
+                map.put(nums[i], 1);
+        }
+        int maxKey = nums[0];
+        int maxValue = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxValue) {
+                maxValue = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+        return maxKey;
     }
 }
