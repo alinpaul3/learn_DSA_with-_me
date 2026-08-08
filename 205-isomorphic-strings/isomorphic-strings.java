4d31d9f6fab27357e888ed57ae2744cc83781645
Alina Paul <145111333+alinpaul3@users.noreply.github.com>
2026-08-08 16:58:57 +0530

    Time: 6 ms (89.42%) | Memory: 43.7 MB (87.17%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> mapST=new HashMap<>();
-        HashMap<Character, Character> mapTS=new HashMap<>();
-        if(s.length()!=t.length()) return false;
-        for(int i=0;i<s.length();i++){
-            char c1=s.charAt(i);
-            char c2=t.charAt(i);
-            if(mapST.containsKey(c1)){
-                if(mapST.get(c1)!=c2) return false;
-            }
-            else mapST.put(c1,c2);
-            if(mapTS.containsKey(c2)){
-                if(mapTS.get(c2)!=c1)return false;
-            }
-            else mapTS.put(c2,c1);
+        if (s.length() != t.length())
+            return false;
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            // s -> t
+            if (mapS[a] != 0 && mapS[a] != b)
+                return false;
+            // t -> s
+            if (mapT[b] != 0 && mapT[b] != a)
+                return false;
+            mapS[a] = b;
+            mapT[b] = a;
         }
         return true;
     }
