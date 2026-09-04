4fce67a26b64fe1efa2b143e6391a32c29293d58
Alina Paul <145111333+alinpaul3@users.noreply.github.com>
2026-09-04 11:34:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (32.11%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,26 +15,14 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-         List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);  
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();  
-            TreeNode lastNode = null;  
-
-            for (int i = 0; i < levelSize; i++) {
-                lastNode = queue.poll();  
-
-                if (lastNode.left != null) queue.offer(lastNode.left);
-                if (lastNode.right != null) queue.offer(lastNode.right);
-            }
-
-            result.add(lastNode.val);  
-        }
-
+        List<Integer> result=new ArrayList<>();
+        deepRoot(root,0,result);
         return result;
     }
+    public void deepRoot(TreeNode root, int depth,List<Integer> result){
+        if(root==null)return;
+        if(depth==result.size())result.add(root.val);
+        deepRoot(root.right,depth+1,result);
+        deepRoot(root.left,depth+1,result);
     }
+}
